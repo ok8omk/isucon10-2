48b29bc51cd8197ebe933fc4863e8a72805d5dab
tomoki-oke <tomoki.oke@gmail.com>
2021-08-15 03:00:38 +0900

    asc index

--- webapp/python/app.py
+++ webapp/python/app.py
@@ -171,7 +171,7 @@ def get_chair_search():
     query = f"SELECT COUNT(*) as count FROM chair WHERE {search_condition}"
     count = select_row(query, params)["count"]
 
-    query = f"SELECT * FROM chair WHERE {search_condition} ORDER BY popularity DESC, id ASC LIMIT %s OFFSET %s"
+    query = f"SELECT * FROM chair WHERE {search_condition} ORDER BY minus_popularity ASC, id ASC LIMIT %s OFFSET %s"
     chairs = select_all(query, params + [per_page, per_page * page])
 
     return {"count": count, "chairs": camelize(chairs)}
