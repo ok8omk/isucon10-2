c5371fb243be4134b0887bc54e7f459e9b7073d1
ok8omk <tomoki.oke@gmail.com>
2021-08-14 18:28:53 +0000

    update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git pull
+
 sudo systemctl restart nginx.service
 sudo systemctl restart mysql.service
 sudo systemctl restart isuumo.python.service
+
+cd ~/isuumo/bench && ./bench --target-url http://localhost
